0bc15c1ccd6811d60d860822815736ba97cdccda
Rahul Chauhan <113838908+Rjchauhan18@users.noreply.github.com>
2026-04-20 14:01:33 +0530

    Datetime Bug fix

--- Home.py
+++ Home.py
@@ -149,9 +149,9 @@ def app():
     # plot the graph
     def plot_raw_data():
         fig = go.Figure()
-        fig.add_trace(go.Scatter( x=tickerDf['Datetime'], y=tickerDf['Open'], name="stock_open"))
+        fig.add_trace(go.Scatter( x=tickerDf['Date'], y=tickerDf['Open'], name="stock_open"))
         fig.add_trace(go.Scatter(
-            x=tickerDf['Datetime'], y=tickerDf['Close'], name="stock_close"))
+            x=tickerDf['Date'], y=tickerDf['Close'], name="stock_close"))
         fig.layout.update(
             title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
         st.plotly_chart(fig , use_container_width=True)
